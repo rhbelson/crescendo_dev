fd622a9486bdc4f0b81e1d1ed91f21c4cef17d29
Gav Barosee <gav@Gavs-MacBook-Pro.local>
2019-01-22 20:48:41 +0000

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { animated, interpolate } from "react-spring/hooks";
+import Carousel from "nuka-carousel";
+
+class Card extends React.Component {
+  render() {
+    const { i, x, y, rot, scale, trans, cards, bind, objs } = this.props;
+    const { name, age, distance, text, pics } = objs[i];
+
+    return (
+      <animated.div
+        key={i}
+        style={{
+          transform: interpolate(
+            [x, y],
+            (x, y) => `translate3d(${x}px,${y}px,0)`
+          )
+        }}
+      >
+        <animated.div
+          {...bind(i)}
+          style={{
+            transform: interpolate([rot, scale], trans)
+            // backgroundImage: `url(${cards[i]})`
+          }}
+        >
+          <div className="card">
+            <Carousel>
+              {pics.map(pic => (
+                <img src={pic} alt="profilePicture" />
+              ))}
+            </Carousel>
+            <h2>{name},</h2>
+            <h2>{age}</h2>
+            <h5>{distance}</h5>
+            <h5>{text}</h5>
+          </div>
+        </animated.div>
+      </animated.div>
+    );
+  }
+}
+
+export default Card;
