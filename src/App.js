d35535f730ac191ab798187e26c0d67be2e58d04
Robbie Belson <rhbelson@gmail.com>
2019-04-08 20:53:29 -0500

    urls from firebase storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import Deck from "./components/Deck";
 
-const App = () => 
-<Deck />;
+// const App = () => 
+// <Deck />;
+
+class App extends Component {
+	constructor(props) {
+    	super(props);
+    }
+	render() {
+		return (
+			<Deck />
+			);
+	}
+
+}
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { animated, interpolate } from "react-spring/hooks";
 import Carousel from "nuka-carousel";
+import ReactAudioPlayer from 'react-audio-player';
 
 class Card extends React.Component {
   render() {
@@ -31,9 +32,11 @@ class Card extends React.Component {
               ))}
             </Carousel>
             <h2>{name}</h2>
-            <h2>{tags}</h2>
-            <h5>{distance}</h5>
-            <h5>{text}</h5>
+            <h3>{tags}</h3>
+            <ReactAudioPlayer
+                    src={"https://firebasestorage.googleapis.com/v0/b/crescendo-music2.appspot.com/o/Noname%20-%20Forever%20(ft.%20Ravyn%20Lenae%20and%20Joseph%20Chilliams).mp3?alt=media&token=4ed372fc-cf99-4c6b-a379-bb63ba273f5a"}
+                    controls
+                  />
           </div>
         </animated.div>
       </animated.div>
