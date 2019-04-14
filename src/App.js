18df8026ec7864366e30df475475f6d65c633cdc
Robbie Belson <rhbelson@gmail.com>
2019-04-14 14:36:35 -0500

    navbar built but not implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Deck from "./components/Deck";
+import {Container, Navbar, NavbarBrand, Button, Col} from "reactstrap";
+import { FaCog} from 'react-icons/fa';
+import {IoIosStats} from "react-icons/io";
 // const App = () =>
 // <Deck />;
 
@@ -7,10 +10,36 @@ class App extends Component {
 	constructor(props) {
     	super(props);
     }
+   //  renderNav(){
+   //  	return (
+   //  			<Navbar color="light" light expand="md">
+   //        			<NavbarBrand>Crescendo</NavbarBrand>
+   //        			</Navbar>
+   //  		);
+   //  }
+
+   //  renderDeck() {
+   //  	return (
+			// <Deck />
+			// );
+   //  }
+
 	render() {
-		return (
-			<Deck />
-			);
+		return(
+			// <Container style={{padding:"0px",display:"flex"}}>
+			// <Navbar style={{backgroundColor:"salmon"}}>
+	  //         <Col xs="4">
+	  //           <Button onClick={this.props.toggleSettings} style={{left:"0",backgroundColor:"#577399"}}><FaCog/></Button>
+	  //         </Col>
+	  //         <Col xs="8">
+	  //           <div style={{fontWeight:"bold",color:"#ffffff"}}>Crescendo</div>
+	  //         </Col>
+	  //       </Navbar> 
+	        <Deck/>
+	        // </Container>
+	        
+         );
+
 	}
 
 }
